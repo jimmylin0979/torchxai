4fe84fc73d473a7f315558d13d2e180cdd7a1cc8
jimmylin0979 <jimmylin0979@gmail.com>
2023-02-11 20:41:08 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,27 @@
+import pathlib
 from setuptools import setup, find_packages
 
+#
+version = "0.0.3"
+
+# 
 with open("README.md", "r", encoding="utf-8") as fr:
     long_description = fr.read()
 
+# 
+install_requires = []
+with open("requirements.txt", encoding="utf-8") as fr:
+    for s in fr.readlines():
+        if s[0] == "#": continue
+        if s[0] == " ": continue
+        install_requires.append(s.strip())
+
+# 
 # testpypi: https://test.pypi.org/project/torchxai/0.0.2/
 setup(
     #
     name="torchxai",
-    version="0.0.2",
+    version=version,
     author="jimmylin0979",
     author_email="jimmylin0979@gmail.com",
     description="Visualzation methods that help developers to realize the deep network",
@@ -20,11 +34,11 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    keywords="deep learning, torch, explainable, visualization",
 
     #
-    python_requires=">=3"
+    python_requires=">=3",
     
-    # TODO: install independences, such as torch, tqdm, and so on ...
-    # install_requires=[],
-
+    # install independences, such as torch, tqdm, and so on ...
+    install_requires=install_requires
 )
